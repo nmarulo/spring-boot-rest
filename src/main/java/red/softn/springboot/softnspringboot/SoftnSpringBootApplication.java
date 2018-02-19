d8db119d1cca11b8864db44cab9537e053cd9b41
Nicolás Marulanda P <nicomaru@outlook.com>
2018-02-19 21:34:14 +0100

    Init

---
+++ src/main/java/red/softn/springboot/softnspringboot/SoftnSpringBootApplication.java
@@ -0,0 +1,12 @@
+package red.softn.springboot.softnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftnSpringBootApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SoftnSpringBootApplication.class, args);
+    }
+}
---
+++ src/test/java/red/softn/springboot/softnspringboot/SoftnSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package red.softn.springboot.softnspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftnSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
